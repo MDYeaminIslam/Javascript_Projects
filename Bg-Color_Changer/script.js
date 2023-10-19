b414d519dd6424b982900fc5148096584951e0c5
Md. Yeamin Islam <yeamin15-13529@diu.edu.bd>
2023-10-19 20:33:29 +0600

    Bg-Color_change project added

---
+++ Bg-Color_Changer/script.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll('.button');
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) =>{
+    console.log(e.target);
+
+    switch(e.target.id){
+      case "black":
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        break;
+      case "blue":
+        document.body.style.backgroundColor = "blue";
+        break;
+      case "red":
+        document.body.style.backgroundColor = "red";
+        break;
+      case "green":
+        document.body.style.backgroundColor = "green";
+        break;
+      case "yellow":
+        document.body.style.backgroundColor = "yellow";
+        document.body.style.color = "black";
+        break;
+    }
+  })
+});
